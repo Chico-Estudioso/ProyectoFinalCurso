64cdc9ae57e02ff2cf999726e8307bd917ea0d3f
Chico-Estudioso <mauroserrano01@educarex.es>
2024-06-05 10:40:28 +0200

    Commit 0506

--- src/app/prod-hub/prod-hub.component.ts
+++ src/app/prod-hub/prod-hub.component.ts
@@ -1,7 +1,8 @@
-import { Component, OnInit, ViewChild, TemplateRef } from '@angular/core';
-import { MatDialog, MatDialogRef } from '@angular/material/dialog';
-import { ServicioBasicoService } from '../servicio-basico.service';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { MatDialog } from '@angular/material/dialog';
 import { Producto } from '../producto';
+import { ServicioBasicoService } from '../servicio-basico.service';
 
 @Component({
   selector: 'app-prod-hub',
@@ -10,13 +11,23 @@ import { Producto } from '../producto';
 })
 export class ProdHubComponent implements OnInit {
   productos: Producto[] = [];
-  @ViewChild('modifyProductModal') modifyProductModal!: TemplateRef<any>;
-  dialogRef!: MatDialogRef<any>;
+  modifyProductForm: FormGroup;
+
+  @ViewChild('modifyProductModal') modifyProductModal: any;
 
   constructor(
     private servicioBasico: ServicioBasicoService,
-    private dialog: MatDialog
-  ) {}
+    private dialog: MatDialog,
+    private fb: FormBuilder
+  ) {
+    this.modifyProductForm = this.fb.group({
+      id: [{ value: '', disabled: true }],
+      nombre: ['', Validators.required],
+      descripcion: ['', Validators.required],
+      precio: ['', Validators.required],
+      valoracion: ['', Validators.required],
+    });
+  }
 
   ngOnInit(): void {
     this.servicioBasico.getProductos().subscribe((data) => {
@@ -24,16 +35,36 @@ export class ProdHubComponent implements OnInit {
     });
   }
 
-  openModifyModal(producto: Producto): void {
-    this.dialogRef = this.dialog.open(this.modifyProductModal, {
+  openModifyModal(producto: Producto) {
+    this.modifyProductForm.patchValue(producto);
+    const dialogRef = this.dialog.open(this.modifyProductModal, {
       width: '400px',
       data: { producto },
     });
+
+    dialogRef.afterClosed().subscribe((result) => {
+      if (result) {
+        this.updateProduct(result);
+      }
+    });
   }
 
-  saveChanges(producto: Producto): void {
-    // Lógica para guardar los cambios en el producto
-    console.log('Producto modificado:', producto);
-    this.dialogRef.close();
+  onSubmit(modal: any): void {
+    if (this.modifyProductForm.valid) {
+      const updatedProduct = this.modifyProductForm.getRawValue();
+      this.updateProduct(updatedProduct);
+      modal.close(updatedProduct);
+    }
+  }
+
+  updateProduct(updatedProduct: Producto): void {
+    const index = this.productos.findIndex((p) => p.id === updatedProduct.id);
+    if (index !== -1) {
+      this.productos[index] = updatedProduct;
+    }
+  }
+
+  addToCart(producto: Producto): void {
+    console.log(`Producto añadido al carrito: ${producto.nombre}`);
   }
 }
